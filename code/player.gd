253134982b91977825a46355e063faec58ef6e89
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2021-03-01 14:56:01 -0600

    finished init attack code

--- code/player.gd
+++ code/player.gd
@@ -5,25 +5,27 @@ extends KinematicBody2D
 export var MAX_SPEED = 150
 export var ACCELERATION = 1000
 export var FRICTION = 750
-export var heavy_attack_available_DELAY = 1.0
+
 export var LIGHT_ATTACK_COOLDOWN_TIME = .5
 export var HEAVY_ATTACK_COOLDOWN_TIME = 1.0
-export var HEAVY_ATTACK_CHARGE_TIME = .5
 
-var walking = false
-var attack_available = true
-var heavy_attack_available = false
-var heavy_attack_now = false
-var attack_cooldown = 0.0
-var attack_delay = 0.0
+export var LIGHT_ATTACK_WINDOW = .8
+export var HEAVY_ATTACK_WINDOW = .5
+
+export var LIGHT_ATTACK_DMG = 1
+export var HEAVY_ATTACK_DMG = 2
+
+enum {ATTACK_READY, LIGHT_WINDUP, HEAVY_WINDUP, LIGHT_ATTACKING, HEAVY_ATTACKING, ATTACK_COOLDOWN}
+
+var attack_state = ATTACK_READY
+
+var current_damage = 0
+
 var velocity = Vector2.ZERO
 var mouse_angle = 0.0
 
 onready var center = $center
 onready var animation = $center/AnimationPlayer
-
-export var light_attack_dmg = 1
-export var heavy_attack_dmg = 2
 
 func _ready():
 	$"/root/Global".register_player(self)
@@ -31,27 +33,45 @@ func _ready():
 func _process(delta):
 
 	# COMBAT CODE
-	if attack_available:
-		if Input.is_action_just_pressed("attack"):
+	match attack_state:
+		ATTACK_READY:
+			$Label.text = 'ATTACK_READY'
+			if Input.is_action_pressed("attack"):
+				attack_state = LIGHT_WINDUP
+				# start light to heavy attack transition timer
+				$light_windup_timer.start(LIGHT_ATTACK_WINDOW)
+		
+		LIGHT_WINDUP:
+			$Label.text = 'LIGHT_WINDUP'
 			animation.play("Prep")
-			heavy_attack_available = false
-			$heavy_attack_charge_timer.start(HEAVY_ATTACK_CHARGE_TIME)
-
-		elif heavy_attack_available and Input.is_action_just_released("attack"):  # heavy attack charged up fully
-			$Label.text = 'HEAVY ATTACK'
-			animation.play("Heavy")
-			
-			begin_attack_cooldown(HEAVY_ATTACK_COOLDOWN_TIME)
-			heavy_attack_available = false
-
-		elif Input.is_action_just_released("attack"):  # light attack
-			$Label.text = 'LIGHT ATTACK'
+			if Input.is_action_just_released("attack"):
+				attack_state = LIGHT_ATTACKING
+		
+		LIGHT_ATTACKING:
+			$Label.text = 'LIGHT_ATTACKING'
 			animation.play("Light")
-			
-			$heavy_attack_charge_timer.stop()
-			begin_attack_cooldown(LIGHT_ATTACK_COOLDOWN_TIME)
-
-
+			attack_state = ATTACK_COOLDOWN
+			current_damage = LIGHT_ATTACK_DMG
+			$attack_cooldown_timer.start(LIGHT_ATTACK_COOLDOWN_TIME)
+		
+		HEAVY_WINDUP:
+			# heavy_windup_timer started in _on_light_windup_timer_timeout
+			$Label.text = 'HEAVY_WINDUP'
+			animation.play("Windup")
+			if Input.is_action_just_released("attack"):
+				attack_state = HEAVY_ATTACKING
+		
+		HEAVY_ATTACKING:
+			$Label.text = 'HEAVY_ATTACKING'
+			animation.play("Heavy")
+			attack_state = ATTACK_COOLDOWN
+			current_damage = HEAVY_ATTACK_DMG
+			$attack_cooldown_timer.start(HEAVY_ATTACK_COOLDOWN_TIME)
+		
+		ATTACK_COOLDOWN:
+			$light_windup_timer.stop()
+			$heavy_windup_timer.stop()
+	
 	# MOVEMENT CODE
 	#set input vector based on input strength from x axis (a/d) and y axis (w/s)
 	var input_vector = Vector2.ZERO
@@ -59,8 +79,8 @@ func _process(delta):
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
-		if walking:
-			input_vector = input_vector/2
+#		if walking:
+#			input_vector = input_vector/2
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -69,26 +89,23 @@ func _process(delta):
 	center.rotation_degrees = mouse_angle - 180
 
 
-func begin_attack_cooldown(time):
-	attack_available = false
-	$attack_cooldown_timer.start(time)
-	print("ATTACK_COOLDOWN: ", time)
-
-
-func _on_heavy_attack_charge_timer_timeout():
-	heavy_attack_available = true
-	animation.play("Windup")
-
-
-func _on_heavy_attack_release_timer_timeout():
-	heavy_attack_now = true
-
-
 func _on_attack_cooldown_timer_timeout():
-	$Label.text = 'attack ready'
-	attack_available = true
+	attack_state = ATTACK_READY
+
+
+func _on_light_windup_timer_timeout():
+	if attack_state == LIGHT_WINDUP:
+		$heavy_windup_timer.start(HEAVY_ATTACK_WINDOW)
+		attack_state = HEAVY_WINDUP
+
+
+func _on_heavy_windup_timer_timeout():
+	print("HEAVY_TIMEOUT")
+	if attack_state == HEAVY_WINDUP:
+		attack_state = HEAVY_ATTACKING
 
 
 func _on_hitbox_area_entered(area):
-	area.take_damage(20)
-
+	print("TEST")
+	
+	area.take_damage(current_damage)
