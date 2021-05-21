3c7182db9118dd76b877accd98324463424117a8
Wes-Ray <wesleyray95@gmail.com>
2021-05-20 21:47:17 -0500

    Revert "added main_menu"
    This reverts commit d2daf450d076d5f632cef846147232994ab09041.

--- Base.gd
+++ Base.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
 
-var player_is_dead : bool = false
 
 func _on_player_player_killed():
-	$death_overlay.show()
-#	pause_mode = Node.PAUSE_MODE_STOP
+	$"CanvasLayer/Death Notice".visible = true
 	get_tree().paused = true
+	
