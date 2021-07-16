0543a1ebb0c5e1a4ee2b751ba607787da1a4b08b
Wes-Ray <wesleyray95@gmail.com>
2021-07-15 21:32:06 -0500

    fixed ghosting knight bug

--- npc/knight/state_knight_stagger.gd
+++ npc/knight/state_knight_stagger.gd
@@ -6,6 +6,7 @@ func enter(npc : KinematicBody2D):
 	.enter(npc)
 
 	# GHOST KNIGHT BOOOO
+func run(npc : KinematicBody2D):
 	if not npc.animation_player.is_playing():
 #		npc.recovery_time = npc.heavy_recovery_time
 		return "idle"
