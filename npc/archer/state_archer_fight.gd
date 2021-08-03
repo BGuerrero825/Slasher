5210fdb79a61da1bafdbb705e6b5655249e1de7a
Brian <BGuerrero825@gmail.com>
2021-08-03 15:50:23 -0500

    increased retreat speed

--- npc/archer/state_archer_fight.gd
+++ npc/archer/state_archer_fight.gd
@@ -3,21 +3,15 @@ extends BaseNPCState
 var hesitation_timer : SceneTreeTimer
 var current_rotation : float
 var direction_vector := Vector2.UP
-var retreat_speed := 2
+var speed_multiplier := 1
+var distance_to_player
 
 
 func enter(npc : KinematicBody2D):
 	hesitation_timer = get_tree().create_timer(npc.attack_hesitation_time)
 	npc.randomize_attack_hesitation()
-	#set a movement direction (forward, backward, or still) based on distance from player
-	var distance_to_player = (npc.global_position - npc.player_ref.global_position).length()
-	if distance_to_player < 200:
-		direction_vector = Vector2.DOWN
-	elif distance_to_player > 350:
-		direction_vector = Vector2.UP
-	else:
-		direction_vector = Vector2.ZERO
-
+	distance_to_player = (npc.global_position - npc.player_ref.global_position).length()
+	
 
 # the player is in range and the player is actively fighting the player (advancing, etc)
 func run(npc: KinematicBody2D):
@@ -29,7 +23,16 @@ func run(npc: KinematicBody2D):
 	# movement
 	var player_pos = npc.player_ref.get_position()
 	npc.rotate_towards(player_pos)
-	npc.strafe_move(direction_vector, npc.speed)
+	#set a movement direction (forward, backward, or still) based on distance from player
+	speed_multiplier = 1
+	if distance_to_player < 200:
+		direction_vector = Vector2.DOWN
+		speed_multiplier = 2
+	elif distance_to_player > 350:
+		direction_vector = Vector2.UP
+	else:
+		direction_vector = Vector2.ZERO
+	npc.strafe_move(direction_vector, npc.speed *speed_multiplier)
 	
 	if npc.player_ref.attacking and npc.in_attack_range(player_pos):
 		return "defend"
